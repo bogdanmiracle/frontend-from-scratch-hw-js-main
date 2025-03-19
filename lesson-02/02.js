54ea6da1c5fa92473623b7b9ca260475696bc9c5
bogdanmiracle <bogdanmiracle@gmail.com>
2025-03-19 18:11:00 +0300

    index

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,11 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 7); // Генерация случайного числа от 1 до 6 для первого кубика
+let dice2 = Math.floor(Math.random() * 7); // Генерация случайного числа от 1 до 6 для второго кубика
+let isWinningDouble = (dice1 === dice2) && (dice1 > 3); // Проверка на выигрышный дубль
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +19,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
