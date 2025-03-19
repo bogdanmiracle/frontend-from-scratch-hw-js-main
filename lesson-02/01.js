300768f913199591259723454495416fb4f0e6ff
bogdanmiracle <bogdanmiracle@gmail.com>
2025-03-19 17:43:41 +0300

    index

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -12,7 +12,15 @@ const isAdmin = false
 const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
-
 let isAccess
 
 // your code
+
+if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+    isAccess = true;
+  } else {
+    isAccess = false;
+  }
+  
+  console.log(isAccess); // вывод результата
+
